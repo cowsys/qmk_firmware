3d76fc5ee47436be446bd8f45eda1f14ab7d1fb2
cowsys <cowsys@gmail.com>
2022-12-21 15:51:40 +0900

    fix layer for browser

--- keyboards/xbows/knight_plus/keymaps/default/keymap.c
+++ keyboards/xbows/knight_plus/keymaps/default/keymap.c
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LCTL , KC_LALT                , KC_LGUI , KC_SPC , TD(TD_LCTL_TAB) , TD(TD_RCTL_TAB)   , KC_SPC  , KC_RGUI , TG(1) , TG(2)   , KC_LEFT , KC_DOWN , KC_RGHT) , // 13 keys
 	[1] = LAYOUT(
             KC_ESC  , KC_TRNS , KC_TRNS , KC_TRNS    , KC_TRNS    , KC_TRNS , KC_TRNS , KC_TRNS            , KC_TRNS , KC_CALC , KC_MYCM      , KC_TRNS , KC_TRNS  , NK_TOGG , QK_BOOT ,
-            KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS    , KC_TRNS    , KC_TRNS , /*      , */ KC_MS_L         , KC_MS_D , KC_MS_U , KC_MS_R      , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS ,
-            RGB_TOG , RGB_MOD , RGB_VAI , KC_TRNS    , LGUI(KC_R) , KC_TRNS , /*      , */ KC_TRNS         , KC_DOWN , KC_UP   , KC_TRNS      , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS , KC_TRNS ,
-            KC_TRNS , KC_TRNS , RGB_VAD , LGUI(KC_W) , KC_TRNS    , KC_TRNS , KC_TRNS , LCTL(LSFT(KC_TAB)) , KC_WH_D , KC_WH_U , LCTL(KC_TAB) , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS ,
+            KC_TRNS , KC_TRNS , RGB_VAI , KC_TRNS    , KC_TRNS    , KC_TRNS , /*      , */ KC_MS_L         , KC_MS_D , KC_MS_U , KC_MS_R      , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS ,
+            RGB_TOG , RGB_MOD , RGB_VAD , KC_TRNS    , LGUI(KC_R) , KC_TRNS , /*      , */ KC_TRNS         , KC_DOWN , KC_UP   , KC_TRNS      , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS , KC_TRNS ,
+            KC_TRNS , KC_TRNS , LGUI(KC_S) , LGUI(KC_W) , KC_TRNS    , KC_TRNS , KC_TRNS , LCTL(LSFT(KC_TAB)) , KC_WH_D , KC_WH_U , LCTL(KC_TAB) , KC_TRNS , KC_TRNS  , KC_TRNS , KC_TRNS ,
             KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS    , KC_TRNS    , KC_TRNS , KC_TRNS , KC_TRNS            , KC_BTN1 , KC_BTN2 , KC_BTN3      , KC_BTN4 , KC_TRNS  , KC_WREF ,
             KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS    , KC_TRNS    , KC_TRNS , KC_TRNS , KC_TRNS            , KC_TRNS , KC_TRNS , KC_WBAK      , KC_TRNS , KC_WFWD) ,
 	[2] = LAYOUT(
