16801b41f99fc29d83af0d55c0e152f2905aa68c
cowsys <cowsys@gmail.com>
2023-04-28 08:52:36 +0900

    add configs for function key

--- keyboards/xbows/knight_plus/keymaps/default/keymap.c
+++ keyboards/xbows/knight_plus/keymaps/default/keymap.c
@@ -76,8 +76,8 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT( // default layer
-            KC_ESC  , LCTL(LSFT(LGUI(KC_4))) , KC_F2   , KC_GRV , KC_MINS         , KC_EQL            , KC_F6   ,KC_F7    , KC_LBRC , KC_RBRC  , KC_BSLS , KC_F11  , KC_F12     , KC_DEL        , KC_PSCR       , // 15 keys
-            KC_GRV  , KC_1                   , KC_2    , KC_3   , KC_4            , KC_5          /*  , */      , KC_6    , KC_7     , KC_8    , KC_9    , KC_0    , KC_MINS    , KC_EQL        , KC_BSPC       , // 14 keys
+            KC_ESC  , KC_F1 , KC_F2   , KC_GRV , KC_MINS         , KC_EQL            , KC_F6   ,KC_F7    , KC_LBRC , KC_RBRC  , KC_BSLS , KC_F11  , KC_F12     , KC_DEL        , KC_PSCR       , // 15 keys
+            LCTL(LSFT(LGUI(KC_4)))  , KC_1                   , KC_2    , KC_3   , KC_4            , KC_5          /*  , */      , KC_6    , KC_7     , KC_8    , KC_9    , KC_0    , KC_MINS    , KC_EQL        , KC_BSPC       , // 14 keys
             KC_TAB  , KC_Q                   , KC_W    , KC_E   , KC_R            , KC_T           /* , */      , KC_Y    , KC_U     , KC_I    , KC_O    , KC_P    , DELVE_NEXT , DELVE_STEPIN  , DELVE_STEPOUT , DELVE_CONTINUE      , // 15 keys
             KC_CAPS , KC_A                   , KC_S    , KC_D   , KC_F            , KC_G              , KC_BSPC , KC_H    , KC_J     , KC_K    , KC_L    , KC_SCLN , KC_QUOT    , KC_ENT        , TG(3)         , // 15 keys
             KC_LSFT , KC_Z                   , KC_X    , KC_C   , KC_V            , KC_B              , KC_ENT  , KC_N    , KC_M     , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT    , KC_UP         , // 14 keys
