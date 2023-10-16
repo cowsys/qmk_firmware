13db61727cad99bdd2bd1829481242e4f1cacb64
cowsys <cowsys@gmail.com>
2023-10-16 22:44:25 +0900

    add layer 6

--- keyboards/xbows/knight_plus/keymaps/default/keymap.c
+++ keyboards/xbows/knight_plus/keymaps/default/keymap.c
@@ -87,12 +87,12 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default layer
     [0] = LAYOUT(
-        LSFT(LGUI(KC_4))       , LSFT(LGUI(KC_5))   , KC_F2   , KC_GRV      , KC_MINS , KC_EQL          , KC_F6           , KC_F7       , KC_LBRC , KC_RBRC , KC_BSLS , KC_F11  , LGUI(LSFT(KC_RBRC)) , KC_DEL       , KC_PSCR       , // 15 keys
-        LCTL(LSFT(LGUI(KC_4))) , KC_1    , KC_2    , KC_3        , KC_4    , KC_5 /*         , */              , KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS             , KC_EQL       , KC_BSPC       , // 14 keys
-        TG(2)                  , KC_Q    , KC_W    , KC_E        , KC_R    , KC_T /*         , */              , KC_Y        , KC_U    , KC_I    , KC_O    , KC_P    , DELVE_NEXT          , DELVE_STEPIN , DELVE_STEPOUT , DELVE_CONTINUE , // 15 keys
-        TG(1)                  , KC_A    , KC_S    , KC_D        , KC_F    , KC_G            , KC_BSPC         , KC_H        , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT             , KC_ENT       , TG(3)         , // 15 keys
-        TG(5)                  , KC_Z    , KC_X    , KC_C        , KC_V    , KC_B            , KC_ENT          , KC_N        , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT             , KC_UP        , // 14 keys
-        KC_LCTL                , KC_LALT , KC_LGUI , MT(MOD_LSFT , KC_SPC) , TD(TD_LCTL_TAB) , TD(TD_RCTL_TAB) , MT(MOD_RSFT , KC_SPC) , KC_RGUI , TG(1)   , TG(2)   , KC_LEFT             , KC_DOWN      , KC_RGHT)      , // 13 keys
+        LSFT(LGUI(KC_4))       , LSFT(LGUI(KC_5)) , KC_F2   , KC_GRV      , KC_MINS , KC_EQL          , KC_F6           , KC_F7       , KC_LBRC , KC_RBRC , KC_BSLS , KC_F11  , LGUI(LSFT(KC_RBRC)) , TG(6)        , KC_PSCR       , // 15 keys
+        LCTL(LSFT(LGUI(KC_4))) , KC_1             , KC_2    , KC_3        , KC_4    , KC_5 /*         , */              , KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS             , KC_EQL       , KC_BSPC       , // 14 keys
+        TG(2)                  , KC_Q             , KC_W    , KC_E        , KC_R    , KC_T /*         , */              , KC_Y        , KC_U    , KC_I    , KC_O    , KC_P    , DELVE_NEXT          , DELVE_STEPIN , DELVE_STEPOUT , DELVE_CONTINUE , // 15 keys
+        TG(1)                  , KC_A             , KC_S    , KC_D        , KC_F    , KC_G            , KC_BSPC         , KC_H        , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT             , KC_ENT       , TG(3)         , // 15 keys
+        TG(5)                  , KC_Z             , KC_X    , KC_C        , KC_V    , KC_B            , KC_ENT          , KC_N        , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT             , KC_UP        , // 14 keys
+        KC_LCTL                , KC_LALT          , KC_LGUI , MT(MOD_LSFT , KC_SPC) , TD(TD_LCTL_TAB) , TD(TD_RCTL_TAB) , MT(MOD_RSFT , KC_SPC) , KC_RGUI , TG(1)   , TG(2)   , KC_LEFT             , KC_DOWN      , KC_RGHT)      , // 13 keys
     // layer for browser
     [1] = LAYOUT(
         KC_ESC  , KC_TRNS , KC_TRNS    , KC_TRNS    , LGUI(KC_MINS) , LGUI(KC_EQL) , KC_TRNS , KC_TRNS            , LGUI(KC_MINS) , LGUI(KC_EQL) , KC_MYCM      , KC_TRNS    , KC_TRNS  , NK_TOGG , QK_BOOT , // 15
@@ -132,5 +132,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO                     , KC_NO                    , /*    , */ LCTL(LALT(LCMD(KC_Y))) , LCTL(LALT(LCMD(KC_U))) , LCTL(LALT(LCMD(KC_I))) , LCTL(LALT(LCMD(KC_O))) , KC_NO   , KC_NO           , KC_NO , KC_NO , KC_NO , // 15
         KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO                     , KC_NO                    , KC_NO , LCTL(LALT(LCMD(KC_H)))    , LCTL(LALT(LCMD(KC_J))) , LCTL(LALT(LCMD(KC_K))) , LCTL(LALT(LCMD(KC_L))) , KC_NO   , KC_NO           , KC_NO , KC_NO , // 15
         KC_TRNS , KC_NO , KC_NO , KC_NO , KC_NO                     , KC_NO                    , KC_NO , KC_NO                     , KC_WH_D                , KC_WH_U                , KC_NO                  , KC_NO   , KC_NO           , KC_UP , // 14
-        KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO                     , KC_NO                    , KC_NO , KC_NO                     , KC_NO                  , KC_NO                  , KC_LEFT                , KC_DOWN , KC_RIGHT) // 13
+        KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO                     , KC_NO                    , KC_NO , KC_NO                     , KC_NO                  , KC_NO                  , KC_LEFT                , KC_DOWN , KC_RIGHT), // 13
+    // layer for cleaning keyboard
+    [6] = LAYOUT(
+        KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO     , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   , KC_TRNS , KC_NO , // 15
+        KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , /*    , */  KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO , // 14
+        KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , /*    , */  KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO , KC_NO , // 15
+        KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO     , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO , // 15
+        KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO     , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO   , // 14
+        KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO     , KC_NO , KC_NO , KC_NO , KC_NO , ) // 13
 };
